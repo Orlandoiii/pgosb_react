de9d50619c1fd92a05617da438f9b0aab328582e
Orlando Torres R <orlandoenriqueiii@gmail.com>
2024-06-24 10:51:56 -0400

    forma de estacion

--- src/ui/components/Stations/Forms/StationForm.jsx
+++ src/ui/components/Stations/Forms/StationForm.jsx
@@ -4,16 +4,11 @@ import { useForm } from "react-hook-form";
 import SelectWithSearch from "../../../core/inputs/SelectWithSearch";
 import Input from "../../../core/inputs/Input";
 import FormHiddenButton from "../../../core/buttons/FormHiddenButton";
+import AddInput from "../../../core/inputs/AddInput";
 
-const brands = ["Ford", "Toyota", "Chevrolet"];
 
-const models = ["Aveo", "Corolla", "Lancer", "Terios"]
 
-const stations = ["Station 1", "Station 2", "Station 3"]
-
-const colors = ["Azul", "Verde", "Amarillo", "Plata", "Dorado"]
-
-const types = ["Tipo 1", "Tipo 2", "Tipo 3", "Tipo 4"]
+const institutions = ["Institución Nombre Largo Para Probar Como se Ve", "Plataforma de Gestion de Operaciones y Servicios Para Bomberos"]
 
 const requiredRule = {
     required: {
@@ -33,30 +28,20 @@ export default function StationForm({ clickSubmitRef, onSubmit }) {
     const { errors, isSubmitted } = formState;
 
 
-    const [brand, setBrand] = useState(currentData?.brand ?? brands[1]);
 
-    const [brandErr, setBrandErr] = useState("");
+    const [institution, setInstution] = useState(institutions[0]);
 
-
-    const [model, setModel] = useState(currentData?.model ?? models[1]);
-
-    const [modelErr, setModelErr] = useState("");
-
-
-    const [station, setStation] = useState(currentData?.station ?? stations[1]);
-
-    const [stationErr, setStationErr] = useState("");
+    const [institutionErr, setInstutionErr] = useState("");
 
 
 
-    const [color, setColor] = useState(currentData?.color ?? colors[1]);
-
-    const [colorErr, setColorErr] = useState("");
 
 
-    const [type, setType] = useState(currentData?.type ?? types[1]);
+    const [regions, setRegions] = useState([]);
 
-    const [typeErr, setTypeErr] = useState("");
+    const [phones, setPhones] = useState([]);
+
+
 
     function handleSubmitInternal(data) {
 
@@ -98,125 +83,103 @@ export default function StationForm({ clickSubmitRef, onSubmit }) {
 
                 <div className="w-full space-y-3 px-2 max-w-[720px]">
 
+                    <SelectWithSearch label={"Institución"}
+                        useDotLabel={true}
+                        options={institutions}
+                        value={institution}
+                        onChange={(v) => { setInstution(v) }}
+                        errMessage={institutionErr}
+                        openUp={false}
+                        onError={(err) => { setInstutionErr(err) }}
+                        useStrongErrColor={isSubmitted} />
+
+
                     <div className="md:flex md:space-x-2">
 
 
-                        <SelectWithSearch label={"Tipo"}
-                            useDotLabel={true}
-                            options={types}
-                            value={type}
-                            onChange={(v) => { setType(v) }}
-                            openUp={true}
-                            onError={(err) => { setTypeErr(err) }}
-                            useStrongErrColor={isSubmitted} />
-
-                        <SelectWithSearch label={"Marca"}
-                            useDotLabel={true}
-                            options={brands}
-                            value={brand}
-                            onChange={(v) => { setBrand(v) }}
-                            openUp={true}
-                            onError={(err) => { setBrandErr(err) }}
-                            useStrongErrColor={isSubmitted} />
-
-
-                        <SelectWithSearch label={"Modelo"}
-                            useDotLabel={true}
-                            options={models}
-                            value={model}
-                            onChange={(v) => { setModel(v) }}
-                            openUp={true}
-                            onError={(err) => { setModelErr(err) }}
-                            useStrongErrColor={isSubmitted} />
-
-                    </div>
-
-                    <div className="">
-
-                        <SelectWithSearch label={"Estacion"}
-                            useDotLabel={true}
-                            options={stations}
-                            value={station}
-                            onChange={(v) => { setStation(v) }}
-                            openUp={true}
-                            onError={(err) => { setStationErr(err) }}
-                            useStrongErrColor={isSubmitted} />
-
-                    </div>
-
-                    <div className="md:flex  md:space-x-2">
-
-
-
-                        <div className="md:w-[w-[50%]]">
+                        <div className="md:w-[30%]">
                             <Input
 
                                 register={register}
                                 validationRules={requiredRule}
 
-                                errMessage={errors.alias?.message}
+                                errMessage={errors.code?.message}
                                 useStrongErrColor={isSubmitted}
-
-                                label={"Alias"}
-                                inputName={"alias"}
+                               
+                                label={"Código"}
+                                inputName={"code"}
                                 useDotLabel={true}
-                                placeHolder="El Caballito"
+                                placeHolder="01"
 
                             />
                         </div>
 
-
-
-                        <div className="md:w-[25%]">
-                            <SelectWithSearch label={"Color"}
-                                useDotLabel={true}
-                                options={colors}
-                                value={color}
-                                onChange={(v) => { setColor(v) }}
-                                openUp={true}
-                                onError={(err) => { setColorErr(err) }}
-                                useStrongErrColor={isSubmitted} />
-                        </div>
-
-
-
-                        <div className="md:w-[25%]">
+                        <div className="md:w-[30%]">
 
                             <Input
 
                                 register={register}
                                 validationRules={requiredRule}
 
-                                errMessage={errors.badge?.message}
+                                errMessage={errors.initials?.message}
                                 useStrongErrColor={isSubmitted}
 
-                                label={"Placa"}
-                                inputName={"badge"}
+                                label={"Siglas"}
+                                inputName={"initials"}
                                 useDotLabel={true}
-                                placeHolder="7HW33A"
+                                placeHolder="M01"
 
                             />
                         </div>
 
-                        <div className="md:w-[18%]">
-                            <Input
+                        <AddInput
 
-                                register={register}
-                                validationRules={requiredRule}
-
-                                errMessage={errors.year?.message}
-                                useStrongErrColor={isSubmitted}
-
-                                label={"Año"}
-                                inputName={"year"}
-                                useDotLabel={true}
-                                placeHolder="2022"
-
-                            />
-                        </div>
+                            label={"Regiones"}
+                            inputName={"regions"}
+                            useDotLabel={true}
+                            placeHolder="Region Operativa Ejem:01M"
+                            useStrongErrColor={isSubmitted}
+                            items={regions}
+                            setItems={setRegions}
+                        />
 
                     </div>
 
+
+                    <div className="md:flex md:space-x-2">
+
+                        <Input
+
+                            register={register}
+                            validationRules={requiredRule}
+
+                            errMessage={errors.description?.message}
+                            useStrongErrColor={isSubmitted}
+                            label={"Descripción"}
+                            inputName={"description"}
+                            useDotLabel={true}
+                            placeHolder="Descripción...."
+
+                        />
+
+
+                        <div className="md:w-[37%]">
+
+
+                            <AddInput
+
+                                label={"Teléfonos"}
+                                inputName={"regions"}
+                                useDotLabel={true}
+                                placeHolder="0212-9855489"
+                                useStrongErrColor={isSubmitted}
+                                items={phones}
+                                setItems={setPhones}
+                            />
+
+                        </div>
+
+                    </div>
 
 
 
