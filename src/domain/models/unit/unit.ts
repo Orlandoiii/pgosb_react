ae10e4ed493e437ecd81f86f73f9106388069c67
dasa0 <davidsotoxxi@gmail.com>
2024-07-20 02:18:57 -0400

    avances validaciones

--- src/domain/models/unit/unit.ts
+++ src/domain/models/unit/unit.ts
@@ -1,112 +1,191 @@
-import { z } from "zod";
+import { z } from 'zod'
 
 export const UnitSchemaBasicData = z.object({
     id: z.string().optional(),
-    
 
-    unity_type: 
-    z.string({
-        required_error: 'El tipo de unidad es requerida',
-    })
-    .optional(),
-    
-    make: 
-    z.string({
-        required_error: 'La marca de la unidad es requerida',
-    })
-    .optional(),
+    unity_type: z
+        .string({
+            required_error: 'El tipo de unidad es requerida',
+        })
+        .optional(),
 
-    model: 
-    z.string({
+    make: z
+        .string({
+            required_error: 'La marca de la unidad es requerida',
+        })
+        .optional(),
+
+    model: z.string({
         required_error: 'El modelo de la unidad es obligatorio',
     }),
 
-    station: 
-    z.string({
+    station: z.string({
         required_error: 'El modelo de la unidad es obligatorio',
     }),
 
-    motor_serial: 
-    z.string()
-    .optional().refine((value) => {
-        return !value || value.length >= 8;
-      }, { message: 'Debe ser igual o mayor a 8 caracteres' })
-      .refine((value) => {
-        return value!.length <= 16;
-      }, { message: 'Debe ser menor o igual a 16 caracteres' }),
+    motor_serial: z
+        .string()
+        .optional()
+        .refine(
+            (value) => {
+                return !value || value.length >= 8
+            },
+            { message: 'Debe ser igual o mayor a 8 caracteres' }
+        )
+        .refine(
+            (value) => {
+                return value!.length <= 16
+            },
+            { message: 'Debe ser menor o igual a 16 caracteres' }
+        ),
 
-    vehicle_serial: 
-    z.string()
-    .optional().refine((value) => {
-        return !value || value.length >= 14;
-      }, { message: 'Debe ser igual o mayor a 14 caracteres' })
-      .refine((value) => {
-        return value!.length <= 17;
-      }, { message: 'Debe ser menor o igual a 17 caracteres' }),
-
+    vehicle_serial: z
+        .string()
+        .optional()
+        .refine(
+            (value) => {
+                return !value || value.length >= 14
+            },
+            { message: 'Debe ser igual o mayor a 14 caracteres' }
+        )
+        .refine(
+            (value) => {
+                return value!.length <= 17
+            },
+            { message: 'Debe ser menor o igual a 17 caracteres' }
+        ),
 
     fuel_type: z.string().optional(),
 
-    alias:
-    z.string()
-    .optional().refine((value) => {
-        return !value || value.length >= 4;
-      }, { message: 'Debe ser igual o mayor a 4 caracteres' })
-      .refine((value) => {
-        return value!.length <= 17;
-      }, { message: 'Debe ser menor o igual a 17 caracteres' }),
+    alias: z
+        .string()
+        .optional()
+        .refine(
+            (value) => {
+                return !value || value.length >= 4
+            },
+            { message: 'Debe ser igual o mayor a 4 caracteres' }
+        )
+        .refine(
+            (value) => {
+                return value!.length <= 17
+            },
+            { message: 'Debe ser menor o igual a 17 caracteres' }
+        ),
 
     color: z.string().optional(),
 
-    plate:
-    z.string()
-    .optional().refine((value) => {
-        return !value || value.length >= 6;
-      }, { message: 'Debe ser igual o mayor a 6 caracteres' })
-      .refine((value) => {
-        return value!.length <= 7;
-      }, { message: 'Debe ser menor o igual a 7 caracteres' }),
-
+    plate: z
+        .string()
+        .optional()
+        .refine(
+            (value) => {
+                return !value || value.length >= 6
+            },
+            { message: 'Debe ser igual o mayor a 6 caracteres' }
+        )
+        .refine(
+            (value) => {
+                return value!.length <= 7
+            },
+            { message: 'Debe ser menor o igual a 7 caracteres' }
+        ),
 
     //PENDIENTE POR MEJORAR, SE DEBE VALIDAR QUE EL VALOR SEA MAYOR A 1950 y MENOR AL AÑO ACTUAL 2025
-    year: 
-    z.number()
-    .optional().refine((value) => {
-        return !value || value > 1950
-      }, { message: 'Debe ser mayor a 1950' }).refine((value) => {return !value || value < new Date().getFullYear() , {}})
+    year: z
+        .number()
+        .optional()
+        .refine(
+            (value) => {
+                return !value || value > 1950
+            },
+            { message: 'Debe ser mayor a 1950' }
+        )
+        .refine((value) => {
+            return (
+                !value || value < new Date().getFullYear(),
+                {
+                    message: `Debe ser menor o igual a ${new Date().getFullYear()}`,
+                }
+            )
+        }),
 })
 export type UnitSchemaBasicDataType = z.infer<typeof UnitSchemaBasicData>
 
-
 export const UnitCharacteristicsSchema = z.object({
-    
     purpose: z.string().optional(),
 
     //PEOPLE CAPACITY
-    capacity: 
-    z.string()
-    .optional().refine((value) => {
-        return !value || value.length >= 8;
-      }, { message: 'Debe ser igual o mayor a 8 caracteres' })
-      .refine((value) => {
-        return value!.length <= 16;
-      }, { message: 'Debe ser menor o igual a 16 caracteres' }),
+    capacity: z
+        .string()
+        .optional()
+        .refine(
+            (value) => {
+                return !value || value.length >= 8
+            },
+            { message: 'Debe ser igual o mayor a 8 caracteres' }
+        )
+        .refine(
+            (value) => {
+                return value!.length <= 16
+            },
+            { message: 'Debe ser menor o igual a 16 caracteres' }
+        ),
 
-
-    hurt_capacity: z.string().optional(),
-    doors: z.string().optional(),
+    hurt_capacity: z
+        .number()
+        .optional()
+        .refine(
+            (value) => {
+                return !value || value < 100
+            },
+            { message: 'Debe ser menor a 100' }
+        ),
+    doors: z
+        .number()
+        .optional()
+        .refine(
+            (value) => {
+                return !value || value < 100
+            },
+            { message: 'Debe ser menor a 100' }
+        ),
     performance: z.string().optional(),
 
-    load_capacity: z.string().optional(),
-    water_capacity: z.string().optional(),
-    init_kilometer: z.string().optional(),
+    load_capacity: z
+        .number()
+        .optional()
+        .refine(
+            (value) => {
+                return !value || value < 100
+            },
+            { message: 'Debe ser menor a 100' }
+        ),
+    water_capacity: z
+        .number()
+        .optional()
+        .refine(
+            (value) => {
+                return !value || value <= 10000
+            },
+            { message: 'Debe ser menor o igual a 10.000' }
+        ),
+    init_kilometer: z
+        .number()
+        .optional()
+        .refine(
+            (value) => {
+                return !value || value < 100
+            },
+            { message: 'Debe ser menor a 100' }
+        ),
 
     details: z.array(z.string()).optional(),
     unity_condition: z.string().optional(),
 
     observations: z.string().optional(),
-
 })
 
-export type UnitCharacteristicsSchemaType = z.infer<typeof UnitCharacteristicsSchema>
-
+export type UnitCharacteristicsSchemaType = z.infer<
+    typeof UnitCharacteristicsSchema
+>
--- src/domain/models/user/user.ts
+++ src/domain/models/user/user.ts
@@ -1,32 +1,63 @@
-import { z } from "zod";
-import { MartialStatusTypes } from "../../abstractions/enums/martial_status_type";
-import { Genders } from "../../abstractions/enums/genders";
-
+import { z } from 'zod'
+import { MartialStatusTypes } from '../../abstractions/enums/martial_status_type'
+import { Genders } from '../../abstractions/enums/genders'
 
 export const UserSchemaBasicData = z.object({
     id: z.number().optional(),
-    user_name: z.string().optional(),
-    email: z.string().email().optional(),
-    first_name: z.string().optional(),
-    last_name: z.string().optional(),
-    legal_id: z.string().optional(),
-    phone: z.string().optional(),
-    zip_code: z.string().optional(),
+    user_name: z
+        .string()
+        .min(3, 'Debe ser mayor o igual a 3 caracteres')
+        .optional(),
+    email: z.string().email('El correo no es valido').optional(),
+    first_name: z.string().min(2, 'Debe tener 2 o más caracteres'),
+    last_name: z.string().min(2, 'Debe tener 2 o más caracteres'),
+    legal_id: z
+        .string()
+        .min(6, 'Debe ser mayor o igual a 6 caracteres')
+        .optional(),
+    phone: z
+        .string()
+        .refine(
+            (value) => {
+                return !value || !value.startsWith('0')
+            },
+            { message: 'No debe iniciar con 0' }
+        )
+        .refine(
+            (value) => {
+                return !value || value.length != 12
+            },
+            { message: 'Debe tener 12 caracteres' }
+        )
+        .optional(),
+    zip_code: z.string().length(4, 'Debe tener 4 caracteres').optional(),
     marital_status: z.nativeEnum(MartialStatusTypes),
-    birth_date: z.string().optional(),
+    birth_date: z
+        .string()
+        .refine(
+            (value) => {
+                return (
+                    !value ||
+                    (value.length == 10 &&
+                        Number(value.split('-')[2] ?? '0') > 1940)
+                )
+            },
+            { message: 'El año debe ser mayor a 1940' }
+        )
+        .optional(),
     gender: z.nativeEnum(Genders).optional(),
     user_system: z.boolean().optional(),
-
-})
+}).refine((value) => {
+    return value.user_system && (value.user_name?.length ?? 0) < 3
+}, {message: "Debe ser mayor o igual a 3 caracteres"})
 
 export type UserSchemaBasicDataType = z.infer<typeof UserSchemaBasicData>
-
 
 export const CharacteristicsSchema = z.object({
     skills: z.array(z.string()).optional(),
     allergies: z.array(z.string()).optional(),
-    height: z.number().nullable().optional(),
-    weight: z.number().nullable().optional(),
+    height: z.number().optional(),
+    weight: z.number().optional(),
     blood_type: z.string().optional(),
     shirt_size: z.string().optional(),
     pant_size: z.string().optional(),
@@ -35,23 +66,13 @@ export const CharacteristicsSchema = z.object({
 
 export type CharacteristicsSchemaType = z.infer<typeof CharacteristicsSchema>
 
-
 export const UserIntutionalDataSchema = z.object({
-
     code: z.string().optional(),
     rol: z.string().optional(),
     rank: z.string().optional(),
     institution: z.string().optional(),
     division: z.string().optional(),
     profesion: z.string().optional(),
+})
 
-});
-
-
-const UserSchema = z.object({
-
-
-
-});
-
-
+const UserSchema = z.object({})
