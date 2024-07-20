6b2156449f4d93298836ffa1191e8f031e426790
Raga98 <ragajeremy@gmail.com>
2024-07-20 02:04:17 -0400

    station ts small changes added

--- src/domain/models/stations/station.ts
+++ src/domain/models/stations/station.ts
@@ -2,11 +2,35 @@ import { z } from "zod";
 
 export const StationSchemaBasicData = z.object({
 
-    institution: z.string().optional(),
-    name: z.string().optional(),
-    description: z.string().optional(),
-    phones:  z.array(z.string()).optional(),
-    code: z.string().optional(),
+    institution: 
+    z.string()
+    .optional().refine((value) => {
+        return !value || value.length >= 5;
+      }, { message: 'Debe ser igual o mayor a 5 caracteres' })
+      .refine((value) => {
+        return value!.length <= 100;
+      }, { message: 'Debe ser menor o igual a 100 caracteres' }),
+
+    name: 
+    z.string()
+    .optional().refine((value) => {
+        return !value || value.length >= 5;
+      }, { message: 'Debe ser igual o mayor a 5 caracteres' })
+      .refine((value) => {
+        return value!.length <= 100;
+      }, { message: 'Debe ser menor o igual a 140 caracteres' }),
+
+    description: 
+    z.string()
+    .optional().refine((value) => {
+        return !value || value.length >= 5;
+      }, { message: 'Debe ser igual o mayor a 5 caracteres' })
+      .refine((value) => {
+        return value!.length <= 100;
+      }, { message: 'Debe ser menor o igual a 240 caracteres' }),
+
+    phones:  z.array(z.number()).optional(),
+    code: z.number().optional(),
     abbreviation: z.string().optional(),
     regions: z.string().optional(),
 
