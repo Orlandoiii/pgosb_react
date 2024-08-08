371039162f34352a6992eb95a34a737bde5b2fd5
Orlando Torres R <orlandoenriqueiii@gmail.com>
2024-08-08 17:05:13 -0400

    cookies

--- src/ui/components/Authentication/AuthProvider.jsx
+++ src/ui/components/Authentication/AuthProvider.jsx
@@ -9,10 +9,13 @@ import { jwtDecode } from "jwt-decode";
 
 
 
-const PGOSB_ACCESS_TOKEN_COOKIE = "PGOSB_ACCESS_TOKEN"
-const PGOSB_REFRESH_TOKEN_COOKIE = "PGOSB_REFRESH_TOKEN"
-const PGOSB_SESSION_STATE_COOKIE = "PGOSB_SESSION_STATE"
+// const PGOSB_ACCESS_TOKEN_COOKIE = "PGOSB_ACCESS_TOKEN"
+// const PGOSB_REFRESH_TOKEN_COOKIE = "PGOSB_REFRESH_TOKEN"
+// const PGOSB_SESSION_STATE_COOKIE = "PGOSB_SESSION_STATE"
 const PGOSB_EXPIRES_IN_COOKIE = "PGOSB_EXPIRES_IN"
+const PGOSB_USER_ID_COOKIE = "PGOSB_USER_ID_COOKIE"
+const PGOSB_EXPIRES_DATE = "PGOSB_EXPIRES_DATE"
+
 
 const authInitialState = {
     isAuthenticated: false,
@@ -21,7 +24,9 @@ const authInitialState = {
 };
 
 function authReducer(state, action) {
+
     switch (action.type) {
+
         case 'LOGIN_SUCCESS':
             return { ...state, isAuthenticated: true, userId: action.payload?.userId };
 
@@ -43,79 +48,93 @@ function authReducer(state, action) {
     }
 }
 
-function isTokenExpired(token, expiresIn) {
-    if (!token || !expiresIn) {
-        return true; // Consider missing input as expired
-    }
+// function isTokenExpired(token, expiresIn) {
+//     if (!token || !expiresIn) {
+//         return true; // Consider missing input as expired
+//     }
 
-    try {
-        const decodedToken = jwtDecode(token);
-        const currentTime = Date.now() / 1000; // Current time in seconds
+//     try {
+//         const decodedToken = jwtDecode(token);
+//         const currentTime = Date.now() / 1000; // Current time in seconds
 
-        // Calculate expiration time based on 'iat' (issued at) claim and expiresIn
-        const expirationTime = decodedToken.iat + parseInt(expiresIn);
+//         // Calculate expiration time based on 'iat' (issued at) claim and expiresIn
+//         const expirationTime = decodedToken.iat + parseInt(expiresIn);
 
 
-        const isExpired = expirationTime < currentTime;
+//         const isExpired = expirationTime < currentTime;
 
-        logger.log("EXPIRED:", isExpired);
+//         logger.log("EXPIRED:", isExpired);
 
-        return isExpired
-    } catch (error) {
-        console.error('Error decoding JWT:', error);
-        return true; // Treat decoding errors as expired for safety
-    }
-}
+//         return isExpired
+//     } catch (error) {
+//         console.error('Error decoding JWT:', error);
+//         return true; // Treat decoding errors as expired for safety
+//     }
+// }
 
-function getRemainingTokenExpirationTime(token, expiresIn) {
-    if (!token || !expiresIn) {
-        return 0; // No token or expiration, consider expired
-    }
+// function getRemainingTokenExpirationTime(token, expiresIn) {
+//     if (!token || !expiresIn) {
+//         return 0; // No token or expiration, consider expired
+//     }
 
-    try {
-        const decodedToken = jwtDecode(token);
-        const currentTime = Date.now() / 1000; // Current time in seconds
+//     try {
+//         const decodedToken = jwtDecode(token);
+//         const currentTime = Date.now() / 1000; // Current time in seconds
 
-        // Calculate expiration time based on 'iat' (issued at) claim and expiresIn
-        const expirationTime = decodedToken.iat + expiresIn;
+//         // Calculate expiration time based on 'iat' (issued at) claim and expiresIn
+//         const expirationTime = decodedToken.iat + expiresIn;
 
-        const remainingTime = expirationTime - currentTime;
+//         const remainingTime = expirationTime - currentTime;
 
-        return Math.max(0, remainingTime); // Ensure non-negative result
-    } catch (error) {
-        console.error('Error decoding JWT:', error);
-        return 0; // Treat decoding errors as expired
-    }
-}
+//         return Math.max(0, remainingTime); // Ensure non-negative result
+//     } catch (error) {
+//         console.error('Error decoding JWT:', error);
+//         return 0; // Treat decoding errors as expired
+//     }
+// }
 
 function setCookies(response) {
-    Cookies.set(PGOSB_ACCESS_TOKEN_COOKIE, response.data.access_token, {
-        expires: response.data.expires_in, secure: false
+    // Cookies.set(PGOSB_ACCESS_TOKEN_COOKIE, response.data.access_token, {
+    //     expires: response.data.expires_in, secure: false
+    // })
+    // Cookies.set(PGOSB_REFRESH_TOKEN_COOKIE, response.data.refresh_token, {
+    //     expires: response.data.expires_in, secure: false
+    // })
+    // Cookies.set(PGOSB_SESSION_STATE_COOKIE, response.data.session_state, {
+    //     expires: response.data.expires_in, secure: false
+    // })
+    // Cookies.set(PGOSB_EXPIRES_IN_COOKIE, response.data.expires_in, {
+    //     expires: response.data.expires_in, secure: false
+    // })
+
+
+
+    Cookies.set(PGOSB_EXPIRES_DATE, response.data.expire_in_date, {
+        expires: response.data.expires_in, secure: false, sameSite: 'Strict', path: ''
     })
-    Cookies.set(PGOSB_REFRESH_TOKEN_COOKIE, response.data.refresh_token, {
-        expires: response.data.expires_in, secure: false
-    })
-    Cookies.set(PGOSB_SESSION_STATE_COOKIE, response.data.session_state, {
-        expires: response.data.expires_in, secure: false
-    })
+
     Cookies.set(PGOSB_EXPIRES_IN_COOKIE, response.data.expires_in, {
-        expires: response.data.expires_in, secure: false
+        expires: response.data.expires_in, secure: false, sameSite: 'Strict', path: ''
     })
+
+    Cookies.set(PGOSB_USER_ID_COOKIE, response.data.user_id, {
+        expires: response.data.expires_in, secure: false, sameSite: 'Strict', path: ''
+    })
+
+
 }
 
 function clearCookies() {
-    Cookies.set(PGOSB_ACCESS_TOKEN_COOKIE, "", {
-        expires: 0, secure: false
-    })
-    Cookies.set(PGOSB_REFRESH_TOKEN_COOKIE, "", {
-        expires: 0, secure: false
-    })
-    Cookies.set(PGOSB_SESSION_STATE_COOKIE, "", {
-        expires: 0, secure: false
-    })
     Cookies.set(PGOSB_EXPIRES_IN_COOKIE, "", {
         expires: 0, secure: false
     })
+    Cookies.set(PGOSB_USER_ID_COOKIE, "", {
+        expires: 0, secure: false
+    })
+    Cookies.set(PGOSB_EXPIRES_DATE, "", {
+        expires: 0, secure: false
+    })
+
 }
 
 async function refreshToken(config, signal) {
@@ -135,15 +154,19 @@ async function refreshToken(config, signal) {
             throw new Error('Unexpected error during refresh token request');
         }
     } catch (error) {
+
         if (axios.isCancel(error)) {
 
             const cancellationError = new Error('Refresh token request cancelled');
+
             cancellationError.code = 'CANCELLED_REQUEST'; // Add custom error code
+
             throw cancellationError;
 
         } else if (error.response && (error.response.status === 401 || error.response.status === 403)) {
 
             throw new Error('Refresh token is invalid or expired');
+
         } else {
 
             throw error; // Rethrow the error for further handling
@@ -155,42 +178,59 @@ async function logIn(data, config, signal, dispatch) {
         const url = `${config.back_url}/api/v1/auth/login`;
 
         const response = await axios.post(url, data, { signal });
+
         if (response.status === 200) { // Assuming 200 is success
 
-            const userJwt = jwtDecode(response.data.access_token);
-            dispatch({ type: 'LOGIN_SUCCESS', payload: { userId: userJwt?.pgosb_id } });
-            setCookies(response);
+            const pgosbId = response.data.user_id
+
+            //setCookies(response);
+
+            dispatch({ type: 'LOGIN_SUCCESS', payload: { userId: pgosbId } });
+
         } else {
-            clearCookies();
+            //clearCookies();
+
             dispatch({ type: 'LOGIN_FAILURE', payload: "Invalid credentials" });
+
             throw new Error("no se logro login")
         }
     } catch (err) {
         logger.error(err);
+
         if (axios.isCancel(err)) {
+
             return;
+
         }
-        clearCookies();
+        //clearCookies();
+
         dispatch({ type: 'LOGIN_FAILURE', payload: error.message });
+
         throw err
     }
 };
 
 async function logOut(config, signal, dispatch) {
     let captureErr = null;
+
     try {
         const url = `${config.back_url}/api/v1/auth/logout`;
+
         const response = await axios.post(url, null, { signal });
+
         return response;
 
     } catch (error) {
 
-        logger.error(error);
+
         captureErr = error
+
         return {}
         // Handle logout errors gracefully
     } finally {
-        clearCookies()
+
+        //clearCookies()
+
         dispatch({ type: "LOGOUT", payload: captureErr })
     }
 };
@@ -224,93 +264,111 @@ export default function AuthProvider({ children }) {
 
     const logInFunction = async (data, signal) => {
         const configCapture = config
+
         const dispatchCapture = dispatch
+
         return logIn(data, configCapture, signal, dispatchCapture)
     }
 
     const logOutFunction = async (signal) => {
         const configCapture = config
+
         const dispatchCapture = dispatch
+
         return logOut(configCapture, signal, dispatchCapture)
     }
 
 
     useEffect(() => {
 
-        const accesToken = Cookies.get(PGOSB_ACCESS_TOKEN_COOKIE);
+        // const userId = Cookies.get(PGOSB_USER_ID_COOKIE);
 
-        if (!accesToken || accesToken == "") {
-            dispatch({ type: "LOGIN_NOT_AUTHENTICATED", payload: "No tiene token de acceso" })
-            setFirstRender(false);
-            return;
+
+        // if (!userId || userId == "") {
+
+        //     dispatch({ type: "LOGIN_NOT_AUTHENTICATED", payload: "No tiene id de usuario" })
+
+        //     setFirstRender(false);
+
+        //     return;
+        // }
+
+        // // Parse the date string (ISO 8601 format with timezone)
+        // const givenDate = new Date(Cookies.get(PGOSB_EXPIRES_DATE));
+
+        // // Get the current date and time
+        // const currentDate = new Date();
+
+
+        // const isExpired = givenDate > currentDate;
+
+        // const timeDifference = givenDate - currentDate;
+
+        // const timeDifferenceInSeconds = timeDifference / 1000;
+
+
+        const signal = new AbortController();
+
+        setLoading(true);
+
+        refreshToken(config, signal)
+            .then((r) => {
+
+
+                const pgosbId = r.data.user_id
+
+                //setCookies(r);
+
+                dispatch({ type: "LOGIN_AUTHENTICATED", payload: { userId: pgosbId } })
+
+            }).catch(err => {
+
+                if (err.code != "CANCELLED_REQUEST") {
+
+                    //clearCookies()
+
+                    dispatch({ type: "LOGIN_NOT_AUTHENTICATED" })
+                }
+            }).finally(() => {
+
+                setLoading(false);
+
+                setFirstRender(false);
+
+            });
+
+
+        return () => {
+            signal.abort();
+
         }
 
-        const expiresIn = Cookies.get(PGOSB_EXPIRES_IN_COOKIE)
-
-        if (isTokenExpired(accesToken, expiresIn) ||
-            getRemainingTokenExpirationTime(accesToken, expiresIn) < 120) {
-
-            const signal = new AbortController();
-
-            setLoading(true);
-
-            refreshToken(config, signal)
-                .then((r) => {
-                    setCookies(r);
-                    const jwtUser = jwtDecode(r.data.access_token);
-
-                    dispatch({ type: "LOGIN_AUTHENTICATED", payload: { userId: jwtUser.pgosb_id } })
-                }).catch(err => {
-                    if (err.code != "CANCELLED_REQUEST") {
-                        clearCookies()
-                        dispatch({ type: "LOGIN_NOT_AUTHENTICATED" })
-                    }
-                }).finally(() => {
-                    setLoading(false);
-                    setFirstRender(false);
-                });
-
-
-            return () => {
-                signal.abort();
-
-            }
-        } else {
-            const jwtUser = jwtDecode(accesToken);
-            dispatch({ type: "LOGIN_AUTHENTICATED", payload: { userId: jwtUser.pgosb_id } })
-            setFirstRender(false);
-        }
 
     }, []);
 
     useEffect(() => {
         if (state.isAuthenticated) {
 
+            const signal = new AbortController();
 
+            refreshRef.current = setInterval(() => {
+                logger.info("Is Refreshing");
+                refreshToken(config, signal)
+                    .then((r) => {
+                        logger.info("Refres Success");
+                    }).catch(err => {
+                        if (err.code != "CANCELLED_REQUEST") {
+                            //clearCookies()
+                            dispatch({ type: "LOGIN_NOT_AUTHENTICATED" })
+                        }
+                    });
+            }, 180000)
 
-            // const signal = new AbortController();
+            return () => {
 
-
-            // refreshRef.current = setInterval(() => {
-            //     logger.info("Is Refreshing");
-            //     refreshToken(config, signal)
-            //         .then((r) => {
-            //             logger.info("Refres Success");
-
-            //             setCookies(r);
-            //         }).catch(err => {
-            //             if (err.code != "CANCELLED_REQUEST") {
-            //                 clearCookies()
-            //                 dispatch({ type: "LOGIN_NOT_AUTHENTICATED" })
-            //             }
-            //         });
-            // }, 1200000000000000)
-
-            // return () => {
-
-            //     signal.abort();
-            //     clearInterval(refreshRef.current);
-            // }
+                signal.abort();
+                clearInterval(refreshRef.current);
+            }
         }
 
     }, [state.isAuthenticated])
