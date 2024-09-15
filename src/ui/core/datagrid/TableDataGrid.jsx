f23ef051182645b42acb03eee8ce664c0689d1cb
Orlando Torres R <orlandoenriqueiii@gmail.com>
2024-09-15 19:17:28 -0400

    50 pages row set

--- src/ui/core/datagrid/TableDataGrid.jsx
+++ src/ui/core/datagrid/TableDataGrid.jsx
@@ -564,7 +564,7 @@ export default function TableDataGrid({
 
     const [rowSelection, setRowSelection] = useState({})
 
-    const [pagination, setPagination] = useState({ pageIndex: 0, pageSize: 25 })
+    const [pagination, setPagination] = useState({ pageIndex: 0, pageSize: 50 })
 
     const inputSetPageRef = useRef(null)
 
